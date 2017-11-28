fa06916b0088124f59fdc0b4e3db6128b68114b7
SNN <bouatra.said@gmail.com>
2017-11-28 19:52:22 +0100

    init pec2

--- PEC2_SAID_BOUATRA_BELOUAFI.R
+++ PEC2_SAID_BOUATRA_BELOUAFI.R
@@ -1,2 +1,11 @@
 getwd()
 setwd("/Users/saidbouatra/Statistical_software/statist_R_dev_git/StatS_P2")
+
+#   Ejercicio 1: Una técnica operatoria tiene un 4% de complicaciones. 
+# ¿Cuál es la probabilidad de que si se realiza la técnica 96 veces haya 2 complicaciones? 
+
+dbinom(2, size = 96 , prob = 0.04) 
+
+# ¿Cuál es la probabilidad de que si se realiza la técnica 101 veces haya más de 4 complicaciones?  
+
+pbinom(4 , size = 101 , prob = 0.04 , lower.tail = FALSE)
