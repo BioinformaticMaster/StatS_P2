701472a28ad60ded70dea53dfd61fb9602bafe8a
SNN <bouatra.said@gmail.com>
2017-12-03 23:22:09 +0100

    exe apply

--- PEC2_SAID_BOUATRA_BELOUAFI.R
+++ PEC2_SAID_BOUATRA_BELOUAFI.R
@@ -22,29 +22,65 @@ qt(0.05 , 16 , lower.tail = FALSE)
 
 ## Sección 2. Teorema Central del Límite.  
 # Ejercicio 4  (Demostración):  A. Genera 100,000 valores aleatorios de acuerdo a una distribución uniforme con mínimo 1 y máximo 5.
-
+set.seed(1234567)
 a <- runif(100000 , min = 1 , max = 5)
-options(max.print = 999999) # no es necesario en este caso , pero si tuvieramos que visualizar todos los valores.
-
 c <- matrix(a,ncol = 20)
-
-hist(c[,1],col = "gray")
+par(mfrow = c(3,3))
+hist(c[,1],col = "orange")
 hist(c[,2],col = "red")
 hist(c[,3],col = "blue")
-hist(c[,4],col = "orange")
-hist(c[,5], col = "pink")
+hist(c[,4],col = "pink")
+hist(c[,5], col = "yellow")
 
 # C. Crea un vector que guarde la suma de los valores por fila. 
 
 v <- apply(c,1,sum)
-head(c)
-head(v)
-
-
-
+head(v,5)
 #Realiza un histograma con referencia a la distribución normal.
-mean(v)
-sd(v)
-hist(dnorm(v,mean(v),sd(v)))
+par(mfrow = c(1,1))
+hist(v,breaks = "Sturges")
+hist(dnorm(a), mean(a),sd(a),add = TRUE ,col = "red")
+# diagrama cuantil-cuantil (qq.plot) con referencia a la distribución normal.
 
-# diagrama cuantil-cuantil (qq.plot) con referencia a la distribución normal.  
+qqplot(v, labels = FALSE)
+
+
+# Sección 3. Funciones y estructuras de control. 
+# Ejercicio 6 
+check_natural <- function(x) {  
+  divisores <- c()     
+  if (as.integer(x) == x) {   
+    cat(x ," es natural y sus divisores son:","\n")         
+    for (i in (1:x)) {          
+      if (x %% i == 0) {
+    divisores <- c(i)            
+      cat(divisores,sep = "\n")
+      }}     
+  } else { 
+    warning(x," no es un numero natural")
+  }
+  } 
+# ejecutamos dos numeros para verificar la funcion check_natural.
+check_natural(6)
+check_natural(5.5)
+
+# Ejercicio 7
+
+my_fibo <- function(n) {
+  fibonacci <- numeric(n)
+  fibonacci[1] <- fibonacci[2] <- 1
+  for( i in 3:n ) {
+    fibonacci[i] <- fibonacci[i - 2] + fibonacci[i - 1]
+   print(fibonacci[i])
+}}
+my_fibo(12) # solo para verificar la funcion.
+
+# Ejercicio 8: Centrémonos en la familia “Apply”. Explica para qué usamos estas funciones y genera un ejemplo práctico para cada una de ellas.  
+
+
+
+
+
+
+
+
